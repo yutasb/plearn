90631f7812777196b83e47b6872cf1d9c58cb6da
yutasb <shibamoto.yuta@gmail.com>
2020-04-22 17:02:21 +0900

    session#createテスト#35

--- spec/requests/activities_spec.rb
+++ spec/requests/activities_spec.rb
@@ -1,28 +1,16 @@
 require 'rails_helper'
 
+
+def activities
+   
+end
+
 RSpec.describe "Activities", type: :request do
-  before do
-        @user = FactoryBot.create(:user)
-      end
-  describe "GET /activities" do
-    it 'activity一覧画面の表示に成功すること' do
-      user = @user
-      get activities_path
+  describe "/activities/new" do
+    it 'Activityの新規登録画面が表示されること（リクエストが成功すること）' do
+      get new_activity_path
       expect(response).to have_http_status(200)
     end
+    
   end
-  # describe "POST /activities" do
-  #   context "as an authenticated user" do
-  #     before do
-  #       @user = FactoryBot.create(:user)
-  #     end
-  #     context "with valid attributes" do
-  #       it 'adds a activity' do
-  #         expect{
-  #           post activities_path
-  #         }.to change(@user.activities, :count).by(1)
-  #       end      
-  #     end
-  #   end
-  # end
 end
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "/login" do
-    it "ログイン画面が表示されること" do
+  describe "session #create" do
+    it "リクエストが成功すること" do
       get login_path
-      expect(response).to have_http_status(200)
+      # expect(response).to have_http_status(200)
+      assert_response :success
     end
 
-    it "ログインに成功すること" do
+    it "アカウント登録しているユーザーはログインに成功すること" do
       @user = FactoryBot.create(:user)
       post login_path, params: {email:"test1@example.com",password:"password"}
       expect(response).to redirect_to activities_path
     end
 
-    it "アカウント登録していないユーザーはログインできないこと" do
+    it "アカウント登録のないユーザーはログインできず、エラーが表示されること" do
       @user = FactoryBot.create(:user)
       post login_path, params: {email:"noaccount@example.com",password: "password"}
       expect(response.body).to include 'メールアドレスかパスワードが正しくありません'
     end
-    
+
+
   end
 end
