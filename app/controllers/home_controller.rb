30eb4abfe1797a6b85af1018f9c76809d1a6857e
yutasb <shibamoto.yuta@gmail.com>
2020-04-26 17:45:27 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  skip_before_action :login_required
   def top
     render layout: false
     # application.html.erbを適用しない
